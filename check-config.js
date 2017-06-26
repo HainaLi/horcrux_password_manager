43a59140782e8fd4d7d7c6e5b34a58671b652f82
Hannah (Haina) Li <haina.li24@gmail.com>
2017-06-26 15:27:40 -0400

    Delete check-config.js

--- check-config.js
+++
@@ -1,9 +0,0 @@
- CheckConfigNamespace = function() {
-     var fs = require('fs');
-
-     if (fs.existsSync('config.json')) {
-         console.log("config.json present: Account ready for login");
-     } else {
-         console.log("config.json not present: Need to set up an account");
-     }
- }();
